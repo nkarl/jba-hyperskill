5740605ed76cdb84ed528f4ade78fbbf4b034f03
Karl N <nordenk@pm.me>
2021-05-20 10:11:59 -0700

    removed old repo

--- python-developer/02/readlines.py
+++
@@ -1,2 +0,0 @@
-file = open('animals.txt', 'r')
-print((file).read())
--- python-developer/03/cal_salary.py
+++
@@ -1,6 +0,0 @@
-with open('salary.txt') as in_f, \
-     open('salary_year.txt', 'w') as out_f:
-    s = in_f.readlines()
-    for p in s:
-        p = p.strip()
-        out_f.write(f'{int(p) * 12}\n')
